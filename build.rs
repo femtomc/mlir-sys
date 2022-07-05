5f546c317617aef7a8f93a7bfd7b69bc878b4b1d
Yota Toyama <raviqqe@gmail.com>
2022-07-04 23:38:38 -0700

    Static linking (#1)
    * Use static link
    
    * Refactor
    
    * Refactor
    
    * Use --link-static
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Update dependencies
    
    * Update lock files
    
    * Update .gitignore
    
    * Add test workflow
    
    * Add setup.sh
    
    * Disable fail-fast
    
    * Fix linting
    
    * Fix PATH
    
    * Fix lint
    
    * Link libc++ dynamically
    
    * Update dependencies
    
    * Add dependabot configuration
    
    * Fix link order
    
    * Refactor
    
    * Refactor
    
    * Refactor
    
    * Run brew update
    
    * Fix build on Linux

--- build.rs
+++ build.rs
@@ -1,34 +1,71 @@
-/*
-
-   This file is part of mlir-sys. License is MIT.
-
-*/
-
 extern crate bindgen;
 
 use std::env;
+use std::error::Error;
 use std::fs;
-use std::path::PathBuf;
+use std::io;
+use std::path::Path;
 use std::process::Command;
+use std::str;
 
-fn llvmconfigshim(arg: &str) -> String {
-    let call = format!("llvm-config {}", arg);
-    let tg = if cfg!(target_os = "windows") {
-        Command::new("cmd")
-            .args(["/C", &call[..]])
-            .output()
-            .expect("failed to execute process")
-    } else {
-        Command::new("sh")
-            .arg("-c")
-            .arg(&call[..])
-            .output()
-            .expect("failed to execute process")
+fn main() {
+    run().unwrap()
+}
+
+fn run() -> Result<(), Box<dyn Error>> {
+    println!("cargo:rerun-if-changed=wrapper.h");
+    println!("cargo:rustc-link-search={}", llvm_config("--libdir")?);
+
+    for name in fs::read_dir(llvm_config("--libdir")?)?
+        .map(|entry| {
+            Ok(if let Some(name) = entry?.path().file_name() {
+                name.to_str().map(String::from)
+            } else {
+                None
+            })
+        })
+        .collect::<Result<Vec<_>, io::Error>>()?
+        .into_iter()
+        .flatten()
+    {
+        if name == "libMLIRSupport.a"
+            || name.starts_with("libMLIR")
+                && name.ends_with(".a")
+                && !["Main", "Support"]
+                    .iter()
+                    .any(|pattern| name.contains(pattern))
+        {
+            println!(
+                "cargo:rustc-link-lib=static={}",
+                name.trim_start_matches("lib").trim_end_matches(".a")
+            );
+        }
     }
-    .stdout;
-    let mut s = String::from_utf8_lossy(&tg);
-    s.to_mut().pop();
-    return s.to_string();
+
+    for name in llvm_config("--libnames")?.trim().split(' ') {
+        println!(
+            "cargo:rustc-link-lib={}",
+            name.trim_start_matches("lib").trim_end_matches(".a")
+        );
+    }
+
+    for flag in llvm_config("--system-libs")?.trim().split(' ') {
+        println!("cargo:rustc-link-lib={}", flag.trim_start_matches("-l"));
+    }
+
+    if let Some(name) = get_system_libcpp() {
+        println!("cargo:rustc-link-lib={}", name);
+    }
+
+    bindgen::builder()
+        .header("wrapper.h")
+        .clang_arg(format!("-I{}", llvm_config("--includedir")?))
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .unwrap()
+        .write_to_file(Path::new(&env::var("OUT_DIR")?).join("bindings.rs"))?;
+
+    Ok(())
 }
 
 fn get_system_libcpp() -> Option<&'static str> {
@@ -36,56 +73,22 @@ fn get_system_libcpp() -> Option<&'static str> {
         None
     } else if cfg!(target_os = "macos") {
         Some("c++")
-    } else if cfg!(target_os = "freebsd") {
-        Some("c++")
     } else {
         Some("stdc++")
     }
 }
 
-fn llvm_libs() -> Vec<String> {
-    let libdir = llvmconfigshim("--libdir");
-    let paths = fs::read_dir(libdir).unwrap();
-    let names = paths
-        .filter_map(|entry| {
-            entry.ok().and_then(|e| {
-                e.path()
-                    .file_name()
-                    .and_then(|n| n.to_str().map(|s| String::from(s)))
-            })
-        })
-        .collect::<Vec<String>>();
-    return names;
-}
+fn llvm_config(argument: &str) -> Result<String, Box<dyn Error>> {
+    let call = format!("llvm-config --link-static {}", argument);
 
-fn main() {
-    // Build bindings to MLIR C API.
-    println!("cargo:rerun-if-changed=wrapper.h");
-    println!("cargo:rerun-if-changed=build.rs");
-    let includedir = llvmconfigshim("--includedir");
-    let libdir = llvmconfigshim("--libdir");
-    println!("cargo:libdir={}", libdir);
-    println!("cargo:rustc-link-search=all={}", libdir);
-    println!(
-        "cargo:rustc-link-lib=dylib={}",
-        get_system_libcpp().unwrap()
-    );
-    println!("cargo:rustc-link-lib=LLVM");
-    println!("cargo:rustc-link-lib=MLIR");
-    for l in llvm_libs().iter() {
-        if l.contains("libMLIRCAPI") {
-            let len = l.len();
-            println!("cargo:rustc-link-lib={}", l[3..(len - 2)].to_string());
+    Ok(str::from_utf8(
+        &if cfg!(target_os = "windows") {
+            Command::new("cmd").args(["/C", &call]).output()?
+        } else {
+            Command::new("sh").arg("-c").arg(&call).output()?
         }
-    }
-    let bindings = bindgen::builder()
-        .header("wrapper.h")
-        .clang_arg(format!("-I/{}", includedir))
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .generate()
-        .expect("Unable to generate bindings.");
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+        .stdout,
+    )?
+    .trim()
+    .to_string())
 }
--- wrapper.h
+++ wrapper.h
@@ -1,17 +1,11 @@
-/*
-
-   This file is part of mlir-sys. License is MIT.
-
-*/
-
-#include <mlir-c/Support.h>
-#include <mlir-c/IR.h>
-#include <mlir-c/Registration.h>
-#include <mlir-c/Transforms.h>
-#include <mlir-c/Pass.h>
-#include <mlir-c/IntegerSet.h>
-#include <mlir-c/ExecutionEngine.h>
-#include <mlir-c/Diagnostics.h>
-#include <mlir-c/Conversion.h>
-#include <mlir-c/BuiltinTypes.h>
 #include <mlir-c/BuiltinAttributes.h>
+#include <mlir-c/BuiltinTypes.h>
+#include <mlir-c/Conversion.h>
+#include <mlir-c/Diagnostics.h>
+#include <mlir-c/ExecutionEngine.h>
+#include <mlir-c/IntegerSet.h>
+#include <mlir-c/IR.h>
+#include <mlir-c/Pass.h>
+#include <mlir-c/Registration.h>
+#include <mlir-c/Support.h>
+#include <mlir-c/Transforms.h>
