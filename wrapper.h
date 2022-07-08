a2ab0782b55bcf7c13f77222ca77d6f58306cf93
Yota Toyama <raviqqe@gmail.com>
2022-07-07 21:40:33 -0700

    Add all headers (#4)
    * Add all headers
    
    * Add more headers

--- wrapper.h
+++ wrapper.h
@@ -1,9 +1,25 @@
+#include <mlir-c/AffineExpr.h>
+#include <mlir-c/AffineMap.h>
 #include <mlir-c/BuiltinAttributes.h>
 #include <mlir-c/BuiltinTypes.h>
 #include <mlir-c/Conversion.h>
+#include <mlir-c/Debug.h>
 #include <mlir-c/Diagnostics.h>
+#include <mlir-c/Dialect/Async.h>
+#include <mlir-c/Dialect/GPU.h>
+#include <mlir-c/Dialect/Linalg.h>
+#include <mlir-c/Dialect/LLVM.h>
+#include <mlir-c/Dialect/PDL.h>
+#include <mlir-c/Dialect/Quant.h>
+#include <mlir-c/Dialect/SCF.h>
+#include <mlir-c/Dialect/Shape.h>
+#include <mlir-c/Dialect/SparseTensor.h>
+#include <mlir-c/Dialect/Standard.h>
+#include <mlir-c/Dialect/Tensor.h>
 #include <mlir-c/ExecutionEngine.h>
 #include <mlir-c/IntegerSet.h>
+// TODO This should be fixed on LLVM >14.0.6.
+// #include <mlir-c/Interfaces.h>
 #include <mlir-c/IR.h>
 #include <mlir-c/Pass.h>
 #include <mlir-c/Registration.h>
