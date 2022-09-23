f9e9f76c4d3bdef5e961d789a3235199fd9cc193
Yota Toyama <raviqqe@gmail.com>
2022-09-22 20:06:04 -0700

    Refactor CI setup (#10)
    * Fix
    
    * Fix

--- tools/setup.sh
+++ tools/setup.sh
@@ -7,7 +7,4 @@ llvm_version=15
 brew update
 brew install llvm@$llvm_version
 
-llvm_prefix=$(brew --prefix)/opt/llvm@$llvm_version
-
-echo MLIR_SYS_150_PREFIX=$llvm_prefix >>$GITHUB_ENV
-echo PATH=$llvm_prefix/bin:$PATH >>$GITHUB_ENV
+echo MLIR_SYS_150_PREFIX=$(brew --prefix llvm@$llvm_version) >>$GITHUB_ENV
