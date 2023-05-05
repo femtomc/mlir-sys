9de5c1dadd4484b762ea16dde8005839fa275f5e
Yota Toyama <raviqqe@gmail.com>
2023-05-05 21:47:12 +1000

    Remove another directory

--- wrapper.h
+++ wrapper.h
@@ -1,6 +1,5 @@
 #include <mlir-c/AffineExpr.h>
 #include <mlir-c/AffineMap.h>
-#include <mlir-c/Bindings/Python/>
 #include <mlir-c/Bindings/Python/Interop.h>
 #include <mlir-c/BuiltinAttributes.h>
 #include <mlir-c/BuiltinTypes.h>
